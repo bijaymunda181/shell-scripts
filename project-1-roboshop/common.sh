44269ebdc8a2627636e7f82982bbf5256be5d305
Bijay <bijay78082@gmail.com>
2025-10-18 09:58:22 +0530

    Adding funcn_exit_status

--- project-1-roboshop/common.sh
+++ project-1-roboshop/common.sh
@@ -4,9 +4,10 @@ systemctl start ${component} &>>$log
 systemctl enable ${component} &>>$log
 }
 
-funct_exit_status() {
+func_exit_status() {
   if [ $? -eq 0 ]; then
     echo "\e[32m ✅ nginx installed successfully \e[0m"
     else
     echo "\e[31m ❌ nginx installation failed \e[0m"
+    fi
 }

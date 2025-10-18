8ea6b01bce2adfbbbc0e50a64af798657afa0152
Bijay <bijay78082@gmail.com>
2025-10-18 10:08:52 +0530

    Adding funcn_exit_status

--- project-1-roboshop/common.sh
+++ project-1-roboshop/common.sh
@@ -1,5 +1,6 @@
 func_systemd() {
 echo -e "\e[36m >>>>>>>>>>>>>>> starting ${component} <<<<<<<<<<<<<<<\e[0m"
+systemctl demon-reload ${component} &>>$log
 systemctl start ${component} &>>$log
 systemctl enable ${component} &>>$log
 }

2b68d16dbcc6cbd56507031050b102e05b8a1d9d
Bijay <bijay78082@gmail.com>
2025-10-18 09:47:47 +0530

    adding echo $?

--- project-1-roboshop/frontend.sh
+++ project-1-roboshop/frontend.sh
@@ -4,15 +4,19 @@ log=/tmp/roboshop.log
 
 echo -e "\e[36m >>>>>>>>>>>>>>> installing nginx <<<<<<<<<<<<<<<\e[0m"
 yum install nginx -y &>>$log
+echo $?
 
 func_systemd
 
 echo -e "\e[36m >>>>>>>>>>>>>>>>>>>>>>>>>>>>>> removing default nginx content <<<<<<<<<<<<<<<\e[0m"
 rm -rf /usr/share/nginx/html/* &>>$log
+echo $?
 
 echo -e "\e[36m >>>>>>>>>>>>>>>>>>>> downloding roboshop frontent content <<<<<<<<<<<<<<<<</e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>$log
+echo $?
 
 echo -e "\e[36m >>>>>>>>>>>>>>>>>>>>>>>>> unzip the content >>>>>>>>>>>>>>>>/e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>$log
+echo $?

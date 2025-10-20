85004d4c64428d0c40e5767b892bcc8275aaee3c
Bijay <bijay78082@gmail.com>
2025-10-20 22:38:53 +0530

    adding function

--- project-1-roboshop/common.sh
+++ project-1-roboshop/common.sh
@@ -1,7 +1,7 @@
 func_exit_status() {
   if [ $? -eq 0 ]; then
-    echo "\e[32m SUCESS \e[0m"
+    echo -e "\e[32m SUCESS \e[0m"
     else
-      echo "\e[31m FAILURE \e[0m"
+      echo -e "\e[31m FAILURE \e[0m"
       fi
 }

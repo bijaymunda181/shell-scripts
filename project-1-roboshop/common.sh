bdacb5b0dbbddcf7c5feaac2bdcd62cc9cc1a1c6
Bijay <bijay78082@gmail.com>
2025-10-20 22:37:19 +0530

    adding function

--- project-1-roboshop/common.sh
+++ project-1-roboshop/common.sh
@@ -1,1 +1,7 @@
-log=/tmp/log
+func_exit_status() {
+  if [ $? -eq 0 ]; then
+    echo "\e[32m SUCESS \e[0m"
+    else
+      echo "\e[31m FAILURE \e[0m"
+      fi
+}
--- project-1-roboshop/frontend.sh
+++ project-1-roboshop/frontend.sh
@@ -2,21 +2,21 @@ log=/tmp/log
 
 echo -e "\e[36m Installing Nginx\e[0m"
 yum install nginx -y &>>$log
-echo $?
+func_exit_status
 
 echo -e "\e[36m Starting Nginx\e[0m"
 systemctl enable nginx &>>$log
 systemctl start nginx &>>$log
-echo $?
+func_exit_status
 
 echo -e "\e[36m Removing Default Nginx Content\e[0m"
 rm -rf /usr/share/nginx/html/* &>>$log
-echo $?
+func_exit_status
 
 echo -e "\e[36m Downloading Nginx Content\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>$log
-echo $?
+func_exit_status
 echo -e "\e[36m Unzip Frontend content\e[0m"
 cd /usr/share/nginx/html &>>$log
 unzip /tmp/frontend.zip &>>$log
-echo $?
+func_exit_status

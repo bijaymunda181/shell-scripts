d8992fb7f6a576b36429e2e0d87f02ffa19b0774
Bijay <bijay78082@gmail.com>
2025-10-20 21:16:53 +0530

    modifing

--- project-1-roboshop/common.sh
+++ project-1-roboshop/common.sh
@@ -82,7 +82,7 @@ yum install nodejs -y $>>$log
 func_exit_status
 
 echo -e "\e[36m Create mongodb repo\e[0m"
-cp mongo.repo /etc/yum.repos.d/mongo.repo &>>log
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>>$log
 func_exit_status
 func_schema_setup
 

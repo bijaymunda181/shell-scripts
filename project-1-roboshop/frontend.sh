adbfaf52560fb4df0ddec7eb431aefd0b44bb6ac
Bijay <bijay78082@gmail.com>
2025-10-20 22:11:26 +0530

    sintex correcting

--- project-1-roboshop/frontend.sh
+++ project-1-roboshop/frontend.sh
@@ -1,20 +1,20 @@
 echo -e "\e[36m Installing Nginx\e[0m"
-yum install nginx -y &>>/tem/roboshop.log
+yum install nginx -y &>>/tmp/roboshop.log
 echo $?
 
 echo -e "\e[36m Starting Nginx\e[0m"
-systemctl enable nginx &>>/tem/roboshop.log
-systemctl start nginx &>>/tem/roboshop.log
+systmpctl enable nginx &>>/tmp/roboshop.log
+systmpctl start nginx &>>/tmp/roboshop.log
 echo $?
 
 echo -e "\e[36m Removing Default Nginx Content\e[0m"
-rm -rf /usr/share/nginx/html/* &>>/tem/roboshop.log
+rm -rf /usr/share/nginx/html/* &>>/tmp/roboshop.log
 echo $?
 
 echo -e "\e[36m Downloading Nginx Content\e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>/tem/roboshop.log
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/roboshop.log
 echo $?
 echo -e "\e[36m Unzip Frontend content\e[0m"
-cd /usr/share/nginx/html &>>/tem/roboshop.log
-unzip /tmp/frontend.zip &>>/tem/roboshop.log
-echo $? &>>/tem/roboshop.log
+cd /usr/share/nginx/html &>>/tmp/roboshop.log
+unzip /tmp/frontend.zip &>>/tmp/roboshop.log
+echo $? &>>/tmp/roboshop.log

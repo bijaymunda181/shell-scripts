8059266419da352ce1120ac8ba17c132d8704e85
Bijay <bijay78082@gmail.com>
2025-10-21 17:29:55 +0530

    user componenet

---
+++ Learn-func-condition-loops-prints-vars/print.sh
@@ -0,0 +1,16 @@
+# Red      31
+# Green    32
+# Yellow   33
+# Blue     34
+# Meganta  35
+# Cyan     36
+
+
+# syntex
+
+echo -e "\e[31m Hellow World \e[0m"
+echo -e "\e[32m Hellow World \e[0m"
+echo -e "\e[33m Hellow World \e[0m"
+echo -e "\e[34m Hellow World \e[0m
+echo -e "\e[35m Hellow World \e[0m"
+echo -e "\e[36m Hellow World \e[0m"

28ddaa6412524315045094dd8a692658abd66b51
Bijay <bijay78082@gmail.com>
2025-10-21 21:15:55 +0530

    adding user.sh

--- project-1-roboshop/user.sh
+++ project-1-roboshop/user.sh
@@ -1,10 +1,16 @@
-componet=${user}
 source common.sh
-schema_setup=mongodb
-
-
-func_nodejs
+component=user
 
 
 
-
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+yum install nodejs -y
+useradd roboshop
+mkdir /app
+curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
+cd /app
+unzip /tmp/user.zip
+cd /app
+npm install
+yum install mongodb-org-shell -y
+mongo --host MONGODB-SERVER-IPADDRESS </app/schema/user.js

0283cfac93a85088674023a3b3f6b680e1043093
Bijay <bijay78082@gmail.com>
2025-10-22 17:42:25 +0530

    function in roboshop

--- project-1-roboshop/common.sh
+++ project-1-roboshop/common.sh
@@ -72,11 +72,11 @@ func_java() {
 
 func_apppreq
 
-echo -e "\e[36Build ${component} servive\e[0m"
+echo -e "\e[36mBuild ${component} servive\e[0m"
   mvn clean package &>>${log}
   mv target/${component}-1.0.jar ${component}.jar &>>${log}
 
-echo -e "\e[36Install MYSQL client\e[0m"
+echo -e "\e[36mInstall MYSQL client\e[0m"
   yum install mysql -y &>>${log}
   mysql -h mysql.rdevopsb72.online -uroot -pRoboShop@1 < /app/schema/${component}.sql &>>${log}
 

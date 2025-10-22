f1cd828152f4b7ed2c56384e138d47e6bdaf2cce
Bijay <bijay78082@gmail.com>
2025-10-22 17:21:19 +0530

    function in roboshop

--- project-1-roboshop/common.sh
+++ project-1-roboshop/common.sh
@@ -1,4 +1,30 @@
-nodejs() {
+func_apppreq() {
+    echo -e "\e[36mCreating Roboshop ${component}\e[0m"
+    useradd roboshop &>>${log}
+
+    echo -e "\e[36mRemoving the old content\e[0m"
+    rm -rf /app &>>${log}
+
+    echo -e "\e[36mDownloading Application Content\e[0m"
+    curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log}
+
+
+    echo -e "\e[36mExtrating Application Content\e[0m"
+    cd /app
+    unzip /tmp/${component}.zip &>>${log}
+    cd /app
+
+    echo -e "\e[36mCreating Application Directory\e[0m"
+    mkdir /app &>>${log}
+}
+
+func_systemd() {
+    systemctl daemon-reload &>>${log}
+    systemctl enable ${component} &>>${log}
+    systemctl restart ${component} &>>${log}
+}
+
+func_nodejs() {
   log=/tmp/roboshop.log # Its a variable
 
   echo -e "\e[36mCreating ${component} service file\e[0m"
@@ -13,23 +39,8 @@ nodejs() {
   echo -e "\e[36mInstalling Nodejs\e[0m"
   yum install nodejs -y &>>${log}
 
-  echo -e "\e[36mCreating Roboshop ${component}\e[0m"
-  ${component}add roboshop &>>${log}
 
-  echo -e "\e[36mRemoving the old content\e[0m"
-  rm -rf /app &>>${log}
-
-  echo -e "\e[36mCreating Application Directory\e[0m"
-  mkdir /app &>>${log}
-
-  echo -e "\e[36mDownloading Application Content\e[0m"
-  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log}
-
-
-  echo -e "\e[36mExtrating Application Content\e[0m"
-  cd /app
-  unzip /tmp/${component}.zip &>>${log}
-  cd /app
+func_apppreq
 
   echo -e "\e[36mDownload Nodejs Depandences\e[0m"
   npm install &>>${log}
@@ -40,9 +51,28 @@ nodejs() {
   echo -e "\e[36mLoad ${component} schema\e[0m"
   mongo --host mongodb.rdevopsb72.online </app/schema/${component}.js &>>${log}
 
-  echo -e "\e[36mstarting ${component} servive\e[0m"
-  systemctl daemon-reload &>>${log}
-  systemctl enable ${component} &>>${log}
-  systemctl restart ${component} &>>${log}
+  func_systemd
 
+
+}
+
+
+func_java() {
+   echo -e "\e[36mstarting ${component} servive\e[0m"
+  cp ${component}.service /etc/systemd/system/${component}.service
+
+ echo -e "\e[36mInstall maven\e[0m"
+  yum install maven -y
+
+func_apppreq
+
+echo -e "\e[36Build ${component} servive\e[0m"
+  mvn clean package
+  mv target/${component}-1.0.jar ${component}.jar
+
+echo -e "\e[36Install MYSQL client\e[0m"
+  yum install mysql -y
+  mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/schema/${component}.sql
+
+func_systemd
 }

789ddd1cf8011a04088e8ed5dc9e42d8f336f905
Bijay <bijay78082@gmail.com>
2025-10-22 14:56:51 +0530

    adding redirectors

--- project-1-roboshop/catalogue.sh
+++ project-1-roboshop/catalogue.sh
@@ -1,43 +1,43 @@
 echo -e "\e[36mCreating catalog service file\e[0m"
-cp catalogue.service /etc/systemd/system/catalogue.service
+cp catalogue.service /etc/systemd/system/catalogue.service >/tmp/roboshop.log
 
 echo -e "\e[36mCreating Mongodb repo\e[0m"
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+cp mongo.repo /etc/yum.repos.d/mongo.repo>/tmp/roboshop.log
 
 echo -e "\e[36mInstall nodejs Repo\e[0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash >/tmp/roboshop.log
 
 echo -e "\e[36mInstalling Nodejs\e[0m"
-yum install nodejs -y
+yum install nodejs -y >/tmp/roboshop.log
 
 echo -e "\e[36mCreating Roboshop user\e[0m"
-useradd roboshop
+useradd  >/tmp/roboshop.log
 
 echo -e "\e[36Removing the old content\e[0m"
-rm -rf /app
+rm -rf /app >/tmp/roboshop.log
 
 echo -e "\e[36mCreating Application Directory\e[0m"
-mkdir /app
+mkdir /app >/tmp/roboshop.log
 
 echo -e "\e[36m\eDownloading Application Content[0m"
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip >/tmp/roboshop.log
 
 
 echo -e "\e[36mExtrating Application Content the Application Content\e[0m"
 cd /app
-unzip /tmp/catalogue.zip
+unzip /tmp/catalogue.zip >/tmp/roboshop.log
 cd /app
 
 echo -e "\e[36mDownload Nodejs Depandences\e[0m"
-npm install
+npm install >/tmp/roboshop.log
 
 echo -e "\e[36Installing Mongodb Client\e[0m"
-yum install mongodb-org-shell -y
+yum install mongodb-org-shell -y >/tmp/roboshop.log
 
 echo -e "\e[36mLoad catalogue schema\e[0m"
-mongo --host mongodb.rdevopsb72.online </app/schema/catalogue.js
+mongo --host mongodb.rdevopsb72.online </app/schema/catalogue.js >/tmp/roboshop.log
 
 echo -e "\e[36mstarting catalogue servive\e[0m"
-systemctl daemon-reload
-systemctl enable catalogue
-systemctl restart catalogue
+systemctl daemon-reload >/tmp/roboshop.log
+systemctl enable catalogue >/tmp/roboshop.log
+systemctl restart catalogue >/tmp/roboshop.log

07f6a1a3120b1da065336d10fc8a8126fef194bd
Bijay <bijay78082@gmail.com>
2025-10-23 18:04:43 +0530

    add exit 10

--- project-1-roboshop/The-Piller-of-Shell-Scripts/02.Function.sh
+++ project-1-roboshop/The-Piller-of-Shell-Scripts/02.Function.sh
@@ -9,3 +9,15 @@ greeting() {
 }
 greeting
 echo function Exit status - $?
+
+# You declare var in main program, you can access that in function and vice-versa.
+# Function have its own special variables
+
+input() {
+  echo First Argument  - $1
+  echo Second Argument - $2
+  echo All Arguments   - $*
+  echo No of Arguments - $#
+}
+
+input abc 1234

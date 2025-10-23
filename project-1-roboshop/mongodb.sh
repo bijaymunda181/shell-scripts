60c978c44339109a06972159f228c9f886ccfe94
Bijay <bijay78082@gmail.com>
2025-10-23 11:14:51 +0530

    adding Exit Status

--- project-1-roboshop/mongodb.sh
+++ project-1-roboshop/mongodb.sh
@@ -1,5 +1,5 @@
 cp mongo.repo /etc/yum.repos.d/mongo.repo
-yum intall momngodb
+yum install mongodb
 # Update Listen address from 127.0.0.1 to 0.0.0.0
 systemctl enable mongod
 systemctl restart mongod

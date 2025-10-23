dc94be68f780a73650192e3b58200028cef3dfc3
Bijay <bijay78082@gmail.com>
2025-10-23 13:44:16 +0530

    special vars

--- project-1-roboshop/payment.sh
+++ project-1-roboshop/payment.sh
@@ -2,7 +2,7 @@ component=payment
 source common.sh
 rabbitmq_app_password=$1
 if [ -z "${rabbitmq_app_password}" ]; then
-  Input rabbitmq_appUser Password is Missing
+  echo Input rabbitmq_appUser Password is Missing
   exit 2
   fi
 func_python

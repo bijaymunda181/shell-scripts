95936ccf29bd560d8d705b24a3d0fe5ca1ce4875
Bijay <bijay78082@gmail.com>
2025-10-26 10:06:17 +0530

    httpd.sh add

---
+++ project-2-http/common.sh
@@ -0,0 +1,7 @@
+func_exit_status() {
+  if [ $? -eq 0 ]; then
+    echo -e "\e[32mSuccess\e[0m"
+    else
+      echo -e "\e[31m FAILURE\e[0m"
+}
+
---
+++ project-2-http/httpd.sh
@@ -0,0 +1,14 @@
+log=/tmp/http.log
+source common.sh
+echo -e "\e[36m Installing http service\e[0m"
+yum install httpd -y &>>${log}
+func_exit_status
+
+echo -e "\e[36mStarting httpd Service\e[0m"
+systemctl enable httpd &>>${log}
+systemctl start httpd &>>${log}
+func_exit_status
+
+
+
+

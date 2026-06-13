13e04f348b713ff599a14b1580de1f3c77867404
Bijay <bijay78082@gmail.com>
2026-06-13 16:30:02 +0530

    elif condition

--- Learn-shell-scripting-YouTube/09_elif.sh
+++ Learn-shell-scripting-YouTube/09_elif.sh
@@ -8,6 +8,8 @@ then
   elif [ $marks -ge 45 ]
   then
     echo "second Division"
+    elif [ $marks -ge 30 ]; then
+        echo "Third division"
     else
       echo "Failed!!!!!!!!!!!!!!!!!!!!!!!!!"
       fi

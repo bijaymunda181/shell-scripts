96df168bdfc3e2d7a1cbda00ed7399476369e014
Bijay <bijay78082@gmail.com>
2026-06-13 11:25:19 +0530

    variables

--- Learn-shell-scripting-YouTube/03_vardemo.sh
+++ Learn-shell-scripting-YouTube/03_vardemo.sh
@@ -6,3 +6,10 @@ age=25
 echo "my name is  $name and my age is $age"
 #or
 echo "my name is ${name} and my age is ${age}"
+
+
+
+# Print the output of any linux command.
+HOSTNAME=$(hostname)
+echo "my server name is $HOSTNAME"
+

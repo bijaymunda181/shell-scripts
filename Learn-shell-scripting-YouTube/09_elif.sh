f2b165892369f0e420f5df186f97145cc7a5c621
Bijay <bijay78082@gmail.com>
2026-06-13 16:43:29 +0530

    elif condition

--- Learn-shell-scripting-YouTube/09_elif.sh
+++ Learn-shell-scripting-YouTube/09_elif.sh
@@ -2,14 +2,15 @@
 
 read -p "Enter you Total marks: " marks
 
-if [ $marks -ge 60 ]
-then
+if [ $marks -ge 60 ]; then
   echo "Fist Division"
-  elif [ $marks -ge 45 ]
-  then
+
+ elif [ $marks -ge 45 ]; then
     echo "second Division"
-    elif [ $marks -ge 30 ]; then
-        echo "Third division"
-    else
-      echo "Failed!!!!!!!!!!!!!!!!!!!!!!!!!"
-      fi
+
+ elif [ $marks -ge 30 ]; then
+    echo "Third division"
+
+ else
+    echo "Failed!!!!!!!!!!!!!!!!!!!!!!!!!"
+fi

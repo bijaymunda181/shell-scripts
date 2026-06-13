535949eb302ac1d044121e3010195d827f3e4cdf
Bijay <bijay78082@gmail.com>
2026-06-13 11:37:40 +0530

    variables

--- Learn-shell-scripting-YouTube/03_vardemo.sh
+++ Learn-shell-scripting-YouTube/03_vardemo.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Defining Variables
-name="Bijay"
+readonly name="Bijay"
 age=25
 echo "my name is  $name and my age is $age"
 #or
 echo "my name is ${name} and my age is ${age}"
 
 name="munda"
-echo " my new name is $name"
+echo "my new name is $name"
 
 
 

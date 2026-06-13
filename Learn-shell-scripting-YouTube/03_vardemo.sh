64862aef3b0656449bb5f7aca9ef5bc06557372c
Bijay <bijay78082@gmail.com>
2026-06-13 11:27:39 +0530

    variables

--- Learn-shell-scripting-YouTube/03_vardemo.sh
+++ Learn-shell-scripting-YouTube/03_vardemo.sh
@@ -11,5 +11,6 @@ echo "my name is ${name} and my age is ${age}"
 
 # Print the output of any linux command.
 HOSTNAME=$(hostname)
+PWD=$(pwd)
 echo "my server name is $HOSTNAME"
-
+echo "my current working directory is $PWD"

f2586d44f7525a799b98fb51c8dd595bf03102bb
Bijay <bijay78082@gmail.com>
2026-06-18 10:18:32 +0530

    Logical operators

--- Learn-shell-scripting-YouTube/12_logical_or.sh
+++ Learn-shell-scripting-YouTube/12_logical_or.sh
@@ -2,7 +2,7 @@
 
 read -p "Enter your grade: " grade
 
-if [ $grade== "A"] || [ $grade== "B" ]; then
+if [ $grade == "A" ] || [ $grade == "B" ]; then
   echo "You are passed with good grade"
 else
   echo "your grades are not in expected range"

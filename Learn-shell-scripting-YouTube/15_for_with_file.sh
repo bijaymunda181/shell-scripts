68654267283195c8349a026a2c9a78effe25da39
Bijay <bijay78082@gmail.com>
2026-06-24 12:11:23 +0530

    check_file_existance

--- Learn-shell-scripting-YouTube/15_for_with_file.sh
+++ Learn-shell-scripting-YouTube/15_for_with_file.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 FILE="/root/shell-scripts/Learn-shell-scripting-YouTube/server.txt"
+
+if  [ ! -f $FILE ]; then
+  echo "file is not found!!!"
+  exit 1
+fi
+
 for server in $(cat $FILE)
 do
   echo "server is $server"
